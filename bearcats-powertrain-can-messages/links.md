94872444857efcc19a4bfc77cf9076f41fe8d71d
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-03-25 16:15:26 -0400

    init commit

--- bearcats-powertrain-can-messages/links.md
+++
@@ -1,3 +0,0 @@
-# Links to everything that is used from the onedrive
-1. DBC files stored here: [DBC files](https://mailuc.sharepoint.com/:f:/r/sites/g-BearcatElectricVehicle/Shared%20Documents/General/EV%203%20(2024-2025)/Powertrain/DBC%20Files?csf=1&web=1&e=YGdrE6)
-
--- bearcats-powertrain-can-messages/src/csv_to_json.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def csv_to_json(csv_file_path):
-    csv_file_path=pd.DataFrame(pd.read_csv("decoded_can_messages.csv", sep=","))
-    csv_file_path.to_json("decoded_can_messages.json", force_ascii=True, orient='records')
-    return "Complete. Return to the InfluxDB UI."
